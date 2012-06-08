67fb03b8f27d94c5cd0981e68c77f4f42ac9df7e
Go Kojima <gou.kojima@gmail.com>
2012-06-09 01:01:23 +0900

    added jars to run the jetty component into build.xml

--- src/Test.java
+++ src/Test.java
@@ -22,6 +22,13 @@ public class Test extends RouteBuilder{
           }
         })
       .to("file:///Users/go/dev/camel/work/outbox?autoCreate=true");
+    from("jetty:http://localhost:8080/test").process(new Processor(){
+          public void process(Exchange exchange) throws Exception {
+            String value = (String)exchange.getIn().getHeader("value");
+            System.out.println(value);
+            exchange.getOut().setBody("<h1>" + value + "</h1>");
+          }
+      });
   }
   public static void main(String[] args) throws Exception{
     CamelContext context = new DefaultCamelContext();
@@ -29,6 +36,14 @@ public class Test extends RouteBuilder{
     ProducerTemplate template = context.createProducerTemplate();
     context.start();
     template.sendBody("direct:start", "hoge?");
-    context.stop();
+    try{
+      while(true){
+        Thread.sleep(1);
+      }
+    } catch(Exception ex){
+      ex.printStackTrace();
+    } finally {
+      context.stop();
+    }
   }
 }
